9db41e87c0ecb71f1b7ab2534e7540b805e7bf60
Balatwo <redditmonsterofficial@gmail.com>
2025-11-11 12:53:24 -0500

    Sprite

--- player.gd
+++ player.gd
@@ -1,18 +1,11 @@
 extends CharacterBody2D
 
-@export var speed = 200
+@export var speed = 400
 
-func _physics_process(delta):
-	var direction = Vector2.ZERO
-
-	if Input.is_action_pressed("ui_right"):
-		direction.x += 1
-	if Input.is_action_pressed("ui_left"):
-		direction.x -= 1
-	if Input.is_action_pressed("ui_down"):
-		direction.y += 1
-	if Input.is_action_pressed("ui_up"):
-		direction.y -= 1
-
-	velocity = direction.normalized() * speed
+func get_input():
+	var input_direction = Input.get_vector("Left", "Right", "Up", "Down" )
+	velocity = input_direction * speed
+	
+func _physics_process(_delta):
+	get_input()
 	move_and_slide()
